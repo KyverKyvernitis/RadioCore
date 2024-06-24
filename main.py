89b41ce94e49be494202a9d08160a0d039ff1de3
KyverKyvernitis <58307221+KyverKyvernitis@users.noreply.github.com>
2024-06-24 18:48:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ from utils.client import BotPool
 pool = BotPool()
 
 pool.setup()
+
+import discord
+from discord.ext import commands
+client = discord.Client
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord. Activity(type=discord.ActivityType.listening, name='To Music'))
+print('We have logged in as {0.user}'.format(client)) 
